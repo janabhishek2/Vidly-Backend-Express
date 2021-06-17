c07bd7420bbba772719621cf09c0b81a81f97cae
Abhishek <janabhishek2@gmail.com>
2021-06-17 17:37:58 +0530

    Fixed errors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const customers = require("./routes/customers");
 const genres = require("./routes/genres");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
-const invoices = require("./routes/invoices");
+
 const users = require("./routes/users");
 const auth = require("./routes/auth");
 const config = require("config");
@@ -31,7 +31,6 @@ app.use("/api/genres", genres);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
-app.use("/api/checkout", invoices);
 app.use("/api/auth", auth);
 
 app.get("/", (req, res) => {
